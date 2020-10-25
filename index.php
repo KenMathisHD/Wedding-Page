035436bafdd5bb7db4794493b430cf30cb9d504b
Kenneth Simpson <kenneth.mathis.simpson@gmail.com>
2020-10-24 19:53:13 -0500

    trying to fix index issue

--- index.php
+++ index.php
@@ -12,31 +12,29 @@ $includes = $path . "/includes/";
 <html>
 
 <head>
-    <title>Home</title>
+    <title>Our Wedding</title>
 	<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,600&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/css/indexHomePage.css">
-	<meta name="description" content="Marriage of Kenny and Ciara">
-	<meta property="og:title" content="Home" />
-	<meta property="og:description" content="Wedding Homepage" />
-	<meta property="og:site_name" content="Ken-n-ciar.com" />
-	<meta property="og:type" content="website" />
-	<meta property="og:locale" content="en_US" />
-	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
 
-	<main>
-		<div class="backgroundWrap">
-			<img src="/imgs/engPhoto.jpeg" alt="">
-		</div>
-		<div class="fadeInWrap">
+	<div class="backgroundWrap">
+	<img src="/imgs/engPhoto.jpeg" alt="">
+	</div>
+	<div class="fadeInWrap">
 
-			<?php include($includes . "non-home-pagenav.php"); ?>
+		<?php include($includes . "non-home-pagenav.php"); ?>
 
-		</div>
-	</main>
+	</div>
+	
+	
+
+    <footer></footer>
+
+	<!-- <script src="/js/jquery.min.js"></script> -->
+
 </body>
 
 </html>
